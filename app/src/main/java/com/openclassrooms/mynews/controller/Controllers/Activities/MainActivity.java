6e7ad2c9187a8df97fc4bc4d9d5974d553009a80
troturier <thibaultroturier@gmail.com>
2018-05-01 12:27:12 +0200

    Mise à jour de l'architecture de l'application

--- app/src/main/java/com/openclassrooms/mynews/controller/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/openclassrooms/mynews/controller/Controllers/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.mynews.controller;
+package com.openclassrooms.mynews.controller.Controllers.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
