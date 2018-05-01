29c30cffc9d7948238520d9383c70dfb04a7467c
troturier <thibaultroturier@gmail.com>
2018-05-01 15:31:27 +0200

    Mise à jour du nom d'une méthode dans l'interface TopStoriesService

--- app/src/main/java/com/openclassrooms/mynews/Utils/TopStoriesService.java
+++ app/src/main/java/com/openclassrooms/mynews/Utils/TopStoriesService.java
@@ -12,7 +12,7 @@ import retrofit2.http.Path;
 
 public interface TopStoriesService {
     @GET("svc/topstories/v2/{section}.json?514f85e678024ec3a52dd5cb986fdc63")
-    Call<List<TopStories>> getFollowing(@Path("section") String section);
+    Call<List<TopStories>> getTopStories(@Path("section") String section);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/")
