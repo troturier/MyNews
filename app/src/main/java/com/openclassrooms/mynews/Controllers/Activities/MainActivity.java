e5a654f8c952874056576dd23acc58b0f06ebb73
troturier <thibaultroturier@gmail.com>
2018-06-19 15:53:19 +0200

    Ajout d'un Navigation Drawer contenant toutes les catégories disponibles via l'API TopStories du NyTimes

--- app/src/main/java/com/openclassrooms/mynews/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/openclassrooms/mynews/Controllers/Activities/MainActivity.java
@@ -1,16 +1,25 @@
 package com.openclassrooms.mynews.Controllers.Activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.openclassrooms.mynews.Controllers.Fragments.MainFragment;
 import com.openclassrooms.mynews.Controllers.Fragments.SampleFragmentPagerAdapter;
 import com.openclassrooms.mynews.R;
+
+import java.util.Objects;
 
 /**
  * Class for the Main Activity
@@ -20,11 +29,26 @@ public class MainActivity extends AppCompatActivity  {
 
     private MainFragment mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.activity_main_frame_layout);
     public static int tabPos = 0;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerList = findViewById(R.id.navList);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+
+        addDrawerItems();
+        setupDrawer();
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         if(savedInstanceState == null) {
             this.configureAndShowMainFragment();
         }
@@ -55,6 +79,49 @@ public class MainActivity extends AppCompatActivity  {
         });
     }
 
+    private void addDrawerItems() {
+        String[] section = { "Arts", "Automobiles", "Books", "Business", "Fashion", "Food", "Health", "Insider", "Magazine", "Movies", "National", "NYregion", "Obituaries", "Opinion", "Politics", "RealEstate", "Science", "Sports", "SundayReview", "Technology", "Theater", "TMagazine", "Travel", "Upshot", "World"};
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, section);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(MainActivity.this, section[position]+" section selected", Toast.LENGTH_SHORT).show());
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                Objects.requireNonNull(getSupportActionBar()).setTitle("Sections");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                Objects.requireNonNull(getSupportActionBar()).setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
     /**
      * Inflate the menu : adds items to the action bar.
      * @param menu Menu item
@@ -62,6 +129,7 @@ public class MainActivity extends AppCompatActivity  {
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_drawer, menu);
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -75,6 +143,9 @@ public class MainActivity extends AppCompatActivity  {
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()) {
             case R.id.mSearch:
                 Intent intentSearch = new Intent(this, SearchActivity.class);
@@ -86,12 +157,16 @@ public class MainActivity extends AppCompatActivity  {
                 startActivity(intentNotifications);
                 return true;
 
+            case R.id.action_settings:
+                return true;
+
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
         }
+
     }
 
     // -------------------
